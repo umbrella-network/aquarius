79758d3a9dfb059ad7a605bc43346e913cfb5463
jjordan-quantum <constant.space@gmail.com>
2022-03-15 14:24:25 -0700

    add formatting

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,14 +2,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
 import {
-    Keypair,
-    Connection,
-    PublicKey,
-    LAMPORTS_PER_SOL,
-    SystemProgram,
-    TransactionInstruction,
-    Transaction,
-    sendAndConfirmTransaction,
+  Keypair,
+  Connection,
+  PublicKey,
+  LAMPORTS_PER_SOL,
+  SystemProgram,
+  TransactionInstruction,
+  Transaction,
+  sendAndConfirmTransaction,
 } from '@solana/web3.js';
 import fs from 'mz/fs';
 import path from 'path';
@@ -23,147 +23,148 @@ let programId: PublicKey;
 let greetedPubkey: PublicKey;
 const PROGRAM_PATH = path.resolve(__dirname, '../../dist/program');
 const PROGRAM_SO_PATH = path.join(PROGRAM_PATH, 'block_root_mappings.so');
-const PROGRAM_KEYPAIR_PATH = path.join(PROGRAM_PATH, 'block_root_mappings-keypair.json');
+const PROGRAM_KEYPAIR_PATH = path.join(
+  PROGRAM_PATH,
+  'block_root_mappings-keypair.json',
+);
 
 class GreetingAccount {
-    counter = 0;
-    constructor(fields: {counter: number} | undefined = undefined) {
-        if (fields) {
-            this.counter = fields.counter;
-        }
+  counter = 0;
+  constructor(fields: {counter: number} | undefined = undefined) {
+    if (fields) {
+      this.counter = fields.counter;
     }
+  }
 }
 
 const GreetingSchema = new Map([
-    [GreetingAccount, {kind: 'struct', fields: [['counter', 'u32']]}],
+  [GreetingAccount, {kind: 'struct', fields: [['counter', 'u32']]}],
 ]);
 
-const GREETING_SIZE = borsh.serialize(
-    GreetingSchema,
-    new GreetingAccount(),
-).length;
+const GREETING_SIZE = borsh.serialize(GreetingSchema, new GreetingAccount())
+  .length;
 
 export async function establishConnection(): Promise<void> {
-    const rpcUrl = await getRpcUrl();
-    connection = new Connection(rpcUrl, 'confirmed');
-    const version = await connection.getVersion();
-    console.log('Connection to cluster established:', rpcUrl, version);
+  const rpcUrl = await getRpcUrl();
+  connection = new Connection(rpcUrl, 'confirmed');
+  const version = await connection.getVersion();
+  console.log('Connection to cluster established:', rpcUrl, version);
 }
 
 export async function establishPayer(): Promise<void> {
-    let fees = 0;
-    if (!payer) {
-        const {feeCalculator} = await connection.getRecentBlockhash();
-        fees += await connection.getMinimumBalanceForRentExemption(GREETING_SIZE);
-        fees += feeCalculator.lamportsPerSignature * 100; // wag
-        payer = await getPayer();
-    }
+  let fees = 0;
+  if (!payer) {
+    const {feeCalculator} = await connection.getRecentBlockhash();
+    fees += await connection.getMinimumBalanceForRentExemption(GREETING_SIZE);
+    fees += feeCalculator.lamportsPerSignature * 100; // wag
+    payer = await getPayer();
+  }
 
-    let lamports = await connection.getBalance(payer.publicKey);
-    if (lamports < fees) {
-        const sig = await connection.requestAirdrop(
-            payer.publicKey,
-            fees - lamports,
-        );
-        await connection.confirmTransaction(sig);
-        lamports = await connection.getBalance(payer.publicKey);
-    }
-
-    console.log(
-        'Using account',
-        payer.publicKey.toBase58(),
-        'containing',
-        lamports / LAMPORTS_PER_SOL,
-        'SOL to pay for fees',
+  let lamports = await connection.getBalance(payer.publicKey);
+  if (lamports < fees) {
+    const sig = await connection.requestAirdrop(
+      payer.publicKey,
+      fees - lamports,
     );
+    await connection.confirmTransaction(sig);
+    lamports = await connection.getBalance(payer.publicKey);
+  }
+
+  console.log(
+    'Using account',
+    payer.publicKey.toBase58(),
+    'containing',
+    lamports / LAMPORTS_PER_SOL,
+    'SOL to pay for fees',
+  );
 }
 
 export async function checkProgram(): Promise<void> {
-    try {
-        const programKeypair = await createKeypairFromFile(PROGRAM_KEYPAIR_PATH);
-        programId = programKeypair.publicKey;
-    } catch (err) {
-        const errMsg = (err as Error).message;
-        throw new Error(
-            `Failed to read program keypair at '${PROGRAM_KEYPAIR_PATH}' due to error: ${errMsg}. Program may need to be deployed with \`solana program deploy dist/program/block_root_mappings.so\``,
-        );
-    }
+  try {
+    const programKeypair = await createKeypairFromFile(PROGRAM_KEYPAIR_PATH);
+    programId = programKeypair.publicKey;
+  } catch (err) {
+    const errMsg = (err as Error).message;
+    throw new Error(
+      `Failed to read program keypair at '${PROGRAM_KEYPAIR_PATH}' due to error: ${errMsg}. Program may need to be deployed with \`solana program deploy dist/program/block_root_mappings.so\``,
+    );
+  }
 
-    const programInfo = await connection.getAccountInfo(programId);
-    if (programInfo === null) {
-        if (fs.existsSync(PROGRAM_SO_PATH)) {
-            throw new Error(
-                'Program needs to be deployed with `solana program deploy dist/program/block_root_mappings.so`',
-            );
-        } else {
-            throw new Error('Program needs to be built and deployed');
-        }
-    } else if (!programInfo.executable) {
-        throw new Error(`Program is not executable`);
+  const programInfo = await connection.getAccountInfo(programId);
+  if (programInfo === null) {
+    if (fs.existsSync(PROGRAM_SO_PATH)) {
+      throw new Error(
+        'Program needs to be deployed with `solana program deploy dist/program/block_root_mappings.so`',
+      );
+    } else {
+      throw new Error('Program needs to be built and deployed');
     }
-    console.log(`Using program ${programId.toBase58()}`);
+  } else if (!programInfo.executable) {
+    throw new Error(`Program is not executable`);
+  }
+  console.log(`Using program ${programId.toBase58()}`);
 
-    const GREETING_SEED = 'hello';
-    greetedPubkey = await PublicKey.createWithSeed(
-        payer.publicKey,
-        GREETING_SEED,
-        programId,
+  const GREETING_SEED = 'hello';
+  greetedPubkey = await PublicKey.createWithSeed(
+    payer.publicKey,
+    GREETING_SEED,
+    programId,
+  );
+
+  const greetedAccount = await connection.getAccountInfo(greetedPubkey);
+  if (greetedAccount === null) {
+    console.log(
+      'Creating account',
+      greetedPubkey.toBase58(),
+      'to say hello to',
+    );
+    const lamports = await connection.getMinimumBalanceForRentExemption(
+      GREETING_SIZE,
     );
 
-    const greetedAccount = await connection.getAccountInfo(greetedPubkey);
-    if (greetedAccount === null) {
-        console.log(
-            'Creating account',
-            greetedPubkey.toBase58(),
-            'to say hello to',
-        );
-        const lamports = await connection.getMinimumBalanceForRentExemption(
-            GREETING_SIZE,
-        );
-
-        const transaction = new Transaction().add(
-            SystemProgram.createAccountWithSeed({
-                fromPubkey: payer.publicKey,
-                basePubkey: payer.publicKey,
-                seed: GREETING_SEED,
-                newAccountPubkey: greetedPubkey,
-                lamports,
-                space: GREETING_SIZE,
-                programId,
-            }),
-        );
-        await sendAndConfirmTransaction(connection, transaction, [payer]);
-    }
+    const transaction = new Transaction().add(
+      SystemProgram.createAccountWithSeed({
+        fromPubkey: payer.publicKey,
+        basePubkey: payer.publicKey,
+        seed: GREETING_SEED,
+        newAccountPubkey: greetedPubkey,
+        lamports,
+        space: GREETING_SIZE,
+        programId,
+      }),
+    );
+    await sendAndConfirmTransaction(connection, transaction, [payer]);
+  }
 }
 
 export async function sayHello(): Promise<void> {
-    console.log('Saying hello to', greetedPubkey.toBase58());
-    const instruction = new TransactionInstruction({
-        keys: [{pubkey: greetedPubkey, isSigner: false, isWritable: true}],
-        programId,
-        data: Buffer.alloc(0), // All instructions are hellos
-    });
-    await sendAndConfirmTransaction(
-        connection,
-        new Transaction().add(instruction),
-        [payer],
-    );
+  console.log('Saying hello to', greetedPubkey.toBase58());
+  const instruction = new TransactionInstruction({
+    keys: [{pubkey: greetedPubkey, isSigner: false, isWritable: true}],
+    programId,
+    data: Buffer.alloc(0), // All instructions are hellos
+  });
+  await sendAndConfirmTransaction(
+    connection,
+    new Transaction().add(instruction),
+    [payer],
+  );
 }
 
 export async function reportGreetings(): Promise<void> {
-    const accountInfo = await connection.getAccountInfo(greetedPubkey);
-    if (accountInfo === null) {
-        throw 'Error: cannot find the greeted account';
-    }
-    const greeting = borsh.deserialize(
-        GreetingSchema,
-        GreetingAccount,
-        accountInfo.data,
-    );
-    console.log(
-        greetedPubkey.toBase58(),
-        'has been greeted',
-        greeting.counter,
-        'time(s)',
-    );
+  const accountInfo = await connection.getAccountInfo(greetedPubkey);
+  if (accountInfo === null) {
+    throw 'Error: cannot find the greeted account';
+  }
+  const greeting = borsh.deserialize(
+    GreetingSchema,
+    GreetingAccount,
+    accountInfo.data,
+  );
+  console.log(
+    greetedPubkey.toBase58(),
+    'has been greeted',
+    greeting.counter,
+    'time(s)',
+  );
 }
--- src/client/main.ts
+++ src/client/main.ts
@@ -1,25 +1,25 @@
 import {
-    establishConnection,
-    establishPayer,
-    checkProgram,
-    sayHello,
-    reportGreetings,
+  establishConnection,
+  establishPayer,
+  checkProgram,
+  sayHello,
+  reportGreetings,
 } from './client';
 
 async function main() {
-    console.log("Let's say hello to a Solana account...");
-    await establishConnection();
-    await establishPayer();
-    await checkProgram();
-    await sayHello();
-    await reportGreetings();
-    console.log('Success');
+  console.log("Let's say hello to a Solana account...");
+  await establishConnection();
+  await establishPayer();
+  await checkProgram();
+  await sayHello();
+  await reportGreetings();
+  console.log('Success');
 }
 
 main().then(
-    () => process.exit(),
-    err => {
-        console.error(err);
-        process.exit(-1);
-    },
+  () => process.exit(),
+  err => {
+    console.error(err);
+    process.exit(-1);
+  },
 );
